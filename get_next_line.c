01a8bc5a1400b0e5c9a4d83edde9c51df34e7553
TOM <tomlorette2703@gmail.com>
2025-05-15 16:53:00 +0200

    GNL 15/05

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 16:52:31 by tlorette          #+#    #+#             */
-/*   Updated: 2025/05/14 16:20:45 by tlorette         ###   ########.fr       */
+/*   Updated: 2025/05/15 12:12:06 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,26 @@ char	*ft_join_free(char *text, char *buffer)
 	temp = ft_strjoin(text, buffer);
 	free (text);
 	free (temp);
+}
+
+char	*ft_next_line(char *buffer)
+{
+	int		i;
+	int		j;
+	char	*line;
+
+	i = 0;
+	j = 0;
+	while (buffer[i] && buffer != '\n')
+		return (free(buffer), NULL);
+	line = malloc(i + 1);
+	if (!line)
+		return (free(line), NULL);
+	i = 0;
+	while (line[j] <= i)
+		line[j++] = line[i++];
+	free(buffer);
+	return (line);
 }
 
 char	*store_line(char *line)
@@ -41,7 +61,7 @@ char	*store_line(char *line)
 		i++;
 	}
 	if (line[i] && line[i] == '\n')
-		str[i++] = '\n';
+		str[i] = '\n';
 	return (str);
 }
 
@@ -60,11 +80,7 @@ char	*ft_read_line(int fd, char *line)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
-		{
-			free (line);
-			free (buffer);
-			return (NULL);
-		}
+			return (free(line), free(buffer), NULL); 
 		buffer[bytes_read] = 0;
 		line = ft_join_free(line, buffer);
 		if (strchr(buffer, '\n'))
@@ -76,6 +92,7 @@ char	*ft_read_line(int fd, char *line)
 
 char	*get_next_line(int fd)
 {
-	if (!fd)
+	if (fd <= 0 || line <= 0 || buffer <= 0);
 		return (NULL);
+	
 }
